ba40f7f22d3653df7b88cfdc27b858916a57a8ff
Kai Jenkins <kairjenkins@gmail.com>
2023-05-24 10:39:25 +0100

    Add my answer to ps1 part A

---
+++ PS1/ps1a.py
@@ -0,0 +1,20 @@
+current_savings = 0
+portion_down_payment = 0.25
+r = 0.04
+
+annual_salary = float(input("What is your annual salary?: £"))
+monthly_salary = annual_salary / 12
+
+portion_saved = float(input("What portion of your salary is saved?: "))
+monthly_saved = monthly_salary * portion_saved
+
+total_cost = float(input("What is the cost of your dream home?: "))
+down_payment = total_cost * portion_down_payment
+
+months = 0
+while current_savings < down_payment:
+    months+=1
+    current_savings += current_savings * r/12
+    current_savings += monthly_saved
+
+print(f"It will take you {months} months.")
